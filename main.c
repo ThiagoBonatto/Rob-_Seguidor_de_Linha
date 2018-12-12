13da9da5b29cbda0aa4c135007b6ae93f80680be
ThiagoBonatto <45821827+ThiagoBonatto@users.noreply.github.com>
2018-12-12 15:09:47 -0200

    Add files via upload

--- main.c
+++ main.c
@@ -10,17 +10,17 @@
 #define S4 PD5    // pino digital 5
 #define S5 PD6    // pino digital 6
 
-#define motor1 PB1  // pino digital 9 
+#define motor1 PB1  // pino digital 9
 #define motor2 PB2  // pino digital 10
 
 #define led PB5 // pino digital 13
 
-#define kp 80
+#define kp 80   //definindo valores do do Kp, Kd, Ki e a velocidade inicial dos motores
 #define kd 70
 #define ki 0
 #define velIni 380
 
-bool LS1,LS2,LS3,LS4,LS5; // leitura dos sensores
+bool LS1,LS2,LS3,LS4,LS5; // criação das variáveis
 int D9 , D10;
 int sinalControle;
 int vel1,vel2;
@@ -28,14 +28,14 @@ double erro,erroAnti;
 int proporcional,derivativo,integrador;
 
 
-void leituraSensores(){
+void leituraSensores(){                //leitura dos sensores, ligado nas portas digitais 2, 3, 4, 5, 6
     LS1 = leituraDigitalD(S1);
     LS2 = leituraDigitalD(S2);
     LS3 = leituraDigitalD(S3);
     LS4 = leituraDigitalD(S4);
     LS5 = leituraDigitalD(S5);
 }
-void valorErro(){
+void valorErro(){                       //Função que determina o erro, dependendo do sensor que estiver em cima da faixa
   if(LS1 && LS2 && LS3 && LS4 && !LS5){
     erro = 5;
   }
@@ -50,7 +50,7 @@ void valorErro(){
   }
   else if(LS1 && LS2 && !LS3 && LS4 && LS5){
     erro = 0;
-    
+
   }
   else if(LS1 && !LS2 && !LS3 && LS4 && LS5){
     erro = -1;
@@ -66,20 +66,20 @@ void valorErro(){
   }
 }
 
-void PID(){ //função controle PD
-  
-  proporcional = erro;
+void PID(){ //função controle PID
+
+  proporcional = erro;          //cálculo da parte proporcional, derivativa e integrativa
   derivativo = erro - erroAnti;
   integrador = integrador + erro;
   sinalControle = kp*proporcional + kd*derivativo + ki*integrador;
 
-  if (sinalControle > velIni) {
+  if (sinalControle > velIni) {             //não permite que o sinal de controle fique maior que a Velocidade inicial
     sinalControle = velIni;
   }
-  if (sinalControle < -velIni) {
+  if (sinalControle < -velIni) {            //não permite que o sinal de controle fique menor que a Velocidade inicial
     sinalControle = -velIni;
-  }  
-  if (velIni - sinalControle > 0) { 
+  }
+  if (velIni - sinalControle > 0) {
     escreveAnalogicoD9(velIni - sinalControle);
   } else {
     escreveAnalogicoD9(0);
@@ -90,28 +90,28 @@ void PID(){ //função controle PD
   } else {
     escreveAnalogicoD10(1023);
   }
-  erroAnti = erro;
+  erroAnti = erro;                          //erro antigo é atualizado para o erro atual
 }
 
 int main(){
-  
-  iniciaPWM();// inicia as configurações do pwm nos pinos D9 e D10
 
-  modoPinoD(S1,0); // 0 entrada 
-  modoPinoD(S2,0); // 0 entrada 
-  modoPinoD(S3,0); // 0 entrada 
-  modoPinoD(S4,0); // 0 entrada 
-  modoPinoD(S5,0); // 0 entrada 
+  iniciaPWM();// inicia as configurações do pPWM nos pinos D9 e D10
+
+  modoPinoD(S1,0); // 0 entrada
+  modoPinoD(S2,0); // 0 entrada
+  modoPinoD(S3,0); // 0 entrada
+  modoPinoD(S4,0); // 0 entrada
+  modoPinoD(S5,0); // 0 entrada
   modoPinoB(motor1,1);  // 1 saida
   modoPinoB(motor2,1);  // 1 saida
-  modoPinoB(led,1);     // 1 saida 
+  modoPinoB(led,1);     // 1 saida
 
   sentidoMotor(1); // aceita sentido1 ou sensito2
 
   erroAnti = 0;
   integrador = 0;
 
-  while(1){
+  while(1){                 //loop para funcionamento das funções principais
     leituraSensores();
     valorErro();
     PID();
--- pwm.c
+++ pwm.c
@@ -1,24 +1,24 @@
 #include "pwm.h"
 #include "utilidades.h"
 
-void iniciaPWM(){	// para os pinos digitais 9 e 10
+void iniciaPWM(){	// configura o PWM dos pinos digitais 8 e 9
 	TCCR1A = ((1 << COM0A1) | (1 << COM0B1) | (1 << WGM11) | (1 << WGM10));
-	TCCR1B = ((1 << CS11) | (1 << CS10)); 
+	TCCR1B = ((1 << CS11) | (1 << CS10));
 }
 
 void escreveAnalogicoD9(int valorPWM){
-	OCR1A = valorPWM;     // PWM pino 9
+	OCR1A = valorPWM;     // escreve os valores de 0 a 1023 no pino 8
 }
 
 void escreveAnalogicoD10(int valorPWM){
-	OCR1B = valorPWM;     // PWM pino 9
+	OCR1B = valorPWM;     // escreve os valores de 0 a 1023 no pino 9
 }
 
 void sentidoMotor(bool sentido){
 	modoPinoB(pinHorario,1);	// 1 saida
 	modoPinoB(pinAntiHorario,1);	// 1 saida
   if(sentido == 1) PORTB = PORTB | (1<<pinAntiHorario) & (~(1<<pinHorario));
-  else if(sentido == 0) PORTB = PORTB | (1<<pinHorario) & (~(1<<pinAntiHorario));  
+  else if(sentido == 0) PORTB = PORTB | (1<<pinHorario) & (~(1<<pinAntiHorario));
 	/*if(sentido == "sentido1") PORTB = PORTB | (1<<pinAntiHorario) & (~(1<<pinHorario));
 	else if(sentido == "sentido2") PORTB = PORTB | (1<<pinHorario) & (~(1<<pinAntiHorario));	*/
   //PORTB = (sentido == "sentido1") ? PORTB | (1<<pinAntiHorario) & (~(1<<pinHorario)) :  PORTB | (1<<pinHorario) & (~(1<<pinAntiHorario));
